734f8ef974e6c87a1215ac67bc8a1554a2b50d2a
Mattéo Pappalardo <mat.papp@free.fr>
2023-09-25 16:14:18 +0100

    3 subprojects: Client, TransactionManager, LeaseManager

---
+++ Client/DADTKVClientLib.cs
@@ -0,0 +1,20 @@
+namespace client;
+
+public struct DadInt
+{
+    public string key;
+    public int value;
+}
+
+public class DADTKVClientLib
+{
+    List<DadInt> TxSubmit(string clientId, List<string> dadIntKeysToRead, List<DadInt> dadIntsToWrite)
+    {
+        return null;
+    }
+
+    bool Status()
+    {
+        return false;
+    }
+}
